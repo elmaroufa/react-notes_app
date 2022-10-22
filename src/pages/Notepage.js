d771674a575263eae8229a2059edd6672341a64f
elmaroufa <elmaroufa@gmail.com>
2022-10-22 12:14:49 +0100

    add update and remove note

--- src/pages/Notepage.js
+++ src/pages/Notepage.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import {useParams, Link} from 'react-router-dom';
+import {useParams, Link, useNavigate} from 'react-router-dom';
 //import  notes from '../assets/data';
 import { ReactComponent as ArrowLeft} from '../assets/arrow-left.svg';
 
 const Notepage = () => {
   const params = useParams()
+  const history = useNavigate()
   console.log("myobjetc:", params);
   //let note = notes.find(note => note.id === Number(params.id))
   let noteId = Number(params.id)
@@ -20,18 +21,41 @@ const Notepage = () => {
   let updateNote = async() => {
     await fetch(`http://localhost:8000/notes/${noteId}`,{
      method: 'PUT',
-     header: {
+     headers: {
        'Content-Type' : 'application/json'
      },
      body: JSON.stringify({ ...note, 'updated': new Date()})
     });
   }
+  let deleteNote = async() => {
+    await fetch(`http://localhost:8000/notes/${noteId}`,{
+     method: 'DELETE',
+     headers: {
+       'Content-Type' : 'application/json'
+     },
+     body: JSON.stringify({note})
+    });
+    history('/', {replace:true})
+  }
+
+  let handleSubmit = () => {
+
+    if (noteId !== 'new' && !note.body){
+       deleteNote()
+    } else if (noteId === 'new'){
+      updateNote()
+    }
+    history('/', {replace:true})
+  }
   return (
     <div className="note">
       <div className="note-header">
+        <h3>
         <Link to="/">
-        <h3> <ArrowLeft/></h3>
+         <ArrowLeft onClick={handleSubmit}/>
         </Link>
+        </h3>
+        <button onClick={deleteNote}>DELETE</button>
       </div> 
      <textarea onChange={(e)=> setNote({...note ,'body': e.target.value})} value={note?.body}>
 
